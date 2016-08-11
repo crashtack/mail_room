f477a27f6f284b1b5ff857d62541d3eb50035da5
crashtack <crashtack@gmail.com>
2016-08-11 16:31:13 -0700

    refactoring control loop

---
+++ src/mailroom.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from mailroom_functions import initialize_donor_dict, initialize_state_dict
+from mailroom_functions import list_of_donors,
+
+initialize_donor_dict()
+initialize_state_dict()
+
+
+while True:
+    response = ask_for_input()
+    pass
--- src/mailroom_functions.py
+++ src/mailroom_functions.py
@@ -2,12 +2,32 @@
 ''' main mailroom script file '''
 
 
+def initialize_state_dict():
+    state_0         # this is the quit state
+    state_1 = {'state': 1,
+               'prompt_message': "input donor name",
+               'user_response': '',
+               'valid_responses': state_1_valid_responses,
+               'action': state_1_action,
+               'next_state': 2
+               }        # this is the initial program entry state
+    state_2
+
+
 def initialize_donor_dict():
-    '''function to create the dictionary of dictionary to hold donor information'''
-    John_Doe = {'name': 'John Doe','total_donation': 0,'num_donations': 0,'avg_donation': 0,'last_donation': 0}
-    Neil_Armstrong = {'name': 'Neil Armstrong','total_donation': 23000000,'num_donations': 4,'avg_donation': 5750000,'last_donation': 10000000}
-    Sara_Smith = {'name': 'Sara Smith','total_donation': 100,'num_donations': 2,'avg_donation': 50,'last_donation': 35}
-    Ralf_the_Dog = {'name': 'Ralf the Dog','total_donation': 1000,'num_donations': 1,'avg_donation': 1000,'last_donation': 1000}
+    '''function to create the dictionary of dictionary to
+       hold donor information
+    '''
+    John_Doe = {'name': 'John Doe', 'total_donation': 0, 'num_donations': 0,
+                'avg_donation': 0, 'last_donation': 0}
+    Neil_Armstrong = {'name': 'Neil Armstrong', 'total_donation': 23000000,
+                      'num_donations': 4, 'avg_donation': 5750000,
+                      'last_donation': 10000000}
+    Sara_Smith = {'name': 'Sara Smith', 'total_donation': 100,
+                  'num_donations': 2, 'avg_donation': 50, 'last_donation': 35}
+    Ralf_the_Dog = {'name': 'Ralf the Dog', 'total_donation': 1000,
+                    'num_donations': 1, 'avg_donation': 1000,
+                    'last_donation': 1000}
 
     donor_dict = {'John Doe': John_Doe,
                   'Neil Armstrong': Neil_Armstrong,
